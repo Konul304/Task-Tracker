6138c2139730ef430fb6a2e5c061036201653f08
Konul304 <memmedovakonul270@gmail.com>
2022-04-15 00:43:09 +0400

    tried to make drag and drop work

--- first.js
+++ first.js
@@ -4,11 +4,9 @@ const input = document.querySelector('input');
 const inputBox = document.querySelector('#inputBox');
 const sorting = document.querySelector('#sorting');
 
-sorting.addEventListener('click', (event) => {
+// sorting.addEventListener('click', (event) => {
 
-})
-
-
+// })
 clear.addEventListener('click', (event) => {
     if (input.value != "") {
         input.value = "";
@@ -17,7 +15,6 @@ clear.addEventListener('click', (event) => {
         alert("Enter the value");
     }
 })
-
 
 const List = document.createElement('div');
 List.classList.add('List');
@@ -30,60 +27,56 @@ box.insertBefore(List, inputBox);
 
 enter.addEventListener('click', (event) => {
 
-    if (input.value != ""  && input.value != " ") {
+    if (input.value != "" && input.value != " ") {
 
-        const li = document.createElement('li');
-        const newInput = document.createElement('input')
-        newInput.value = input.value;
-        newInput.classList.add('newInput')
-        let img = document.createElement('img');
-        img.src = "images/Group77.png";
-        img.classList.add('clearItem');
-
-        li.append(newInput)
-        li.append(img);
-        ul.append(li);
-        img.style.boxSizing = 'border-box';
-        List.style.display = 'block';
+        createInput();
         input.value = "";
-
-        img.addEventListener('click', (e) => {
-            li.style.display = 'none';
-            const first = ul.firstChild;
-            console.log(first)
-            if ( li==ul.firstChild) {
-                List.style.display = 'none';
-            }
-        })
+    }
+    else {
+        createInput();
 
     }
-    else{
-        const li = document.createElement('li');
-        const newInput = document.createElement('input')
-        newInput.value = input.value;
-        newInput.classList.add('newInput')
-        let img = document.createElement('img');
-        img.src = "images/Group77.png";
-        img.classList.add('clearItem');
-
-        li.append(newInput)
-        li.append(img);
-        ul.append(li);
-        img.style.boxSizing = 'border-box';
-        List.style.display = 'block';
-
-        img.addEventListener('click', (e) => {
-            li.style.display = 'none';
-            const first = ul.firstChild;
-            console.log(first)
-            if ( li==ul.firstChild) {
-                List.style.display = 'none';
-            }
-        })
-
-    }
-
-
 })
 
+function createInput() {
+    const li = document.createElement('li');
+    const newInput = document.createElement('input')
+    newInput.value = input.value;
+    newInput.classList.add('newInput')
+    let img = document.createElement('img');
+    img.src = "images/Group77.png";
+    img.classList.add('clearItem');
+    li.append(newInput)
+    li.append(img);
+    ul.append(li);
+    img.style.boxSizing = 'border-box';
+    List.style.display = 'block';
+    // li.classList.add('dropzone')
+    // li.draggable = 'true';
+    // li.ondragstart = "event.dataTransfer.setData('text/plain',null)";
+    img.addEventListener('click', (e) => {
+        li.style.display = 'none';
+        const first = ul.firstChild;
+        console.log(first)
+        if (li == ul.firstChild) {
+            List.style.display = 'none';
+        }
+    })
+}
+//     var dragged;
+//     document.addEventListener("dragstart", function (event) {
+//         dragged = event.target;
+//     }, false);
+
+//     document.addEventListener("dragover", function (event) {
+//         event.preventDefault();
+//     }, false);
+
+//     document.addEventListener("drop", function (event) {
+//         event.preventDefault();
+//         if (event.target.className == "dropzone") {
+//             dragged.parentNode.removeChild(dragged);
+//             event.target.appendChild(dragged);
+//         }
+//     }, false);
 
